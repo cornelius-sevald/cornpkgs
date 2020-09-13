b6c1ebdaebd961d54f039494d43343914111daf5
Cornelius Sevald-Krause <cornelius.sevald@protonmail.com>
2020-09-13 02:10:41 +0200

    Remove urn

--- default.nix
+++ default.nix
@@ -17,6 +17,5 @@
   dwm = pkgs.callPackage ./pkgs/dwm { };
   dwmstatus = pkgs.callPackage ./pkgs/dwmstatus { inherit laptop; };
   st = pkgs.callPackage ./pkgs/st { };
-  urn = pkgs.callPackage ./pkgs/urn { };
 }
 
